a6b06865f65c9141e7551e382d4efb4ef12056de
Fiona <spamsuckersunited@gmail.com>
2022-07-21 08:33:59 -0600

    stuffs

--- SniperPOV/SniperPOV/main.cpp
+++ SniperPOV/SniperPOV/main.cpp
@@ -1,12 +1,8 @@
-#include "pch.h"
+gi#include "pch.h"
 
 #include <Windows.h>
-#include <iostream>
-#include <fstream>
-#include <string>
 #include "tfcond.h"
 #include "intrin.h"
-
 
 #pragma comment (lib, "detours.lib")
 #include "detours.h"
@@ -18,52 +14,53 @@ using namespace std;
 auto InCondOffset = 0x4c17d0;
 auto WearableDrawOffset = 0x3227f0;
 auto PlayerShouldDrawOffset = 0x409db0;
-HMODULE base1, base2;
 
 
-typedef bool (__thiscall* oFunction) (void* ecx, ETFCond cond);
-oFunction Function;
+typedef bool(__fastcall* tInCond) (void* ths, ETFCond cond);
+tInCond oInCond;
 
-bool __fastcall CTFPlayerShared_InCond(void* ecx, ETFCond cond) {
+bool __fastcall hInCond(void* ecx, void* edx, ETFCond cond) {
 
-    //auto caller = _ReturnAddress();
+	//auto caller = _ReturnAddress();
 
-    return false;
+	return false;
 
 	//return Function(ecx, cond);
 }
 
 DWORD WINAPI entry(LPVOID lpparam)
-{	
-    //auto sig = find_ida_sig("client.dll", { "55 8B EC 83 EC 08 56 57 8B 7D 08 8B F1 83 FF 20" });
+{
 
-    //auto base = GetModuleHandleA("client.dll");
+	//auto sig = find_ida_sig("client.dll", { "55 8B EC 83 EC 08 56 57 8B 7D 08 8B F1 83 FF 20" });
+	//Function = (oFunction)(sig);
 
 
-    //Function = (oFunction)((DWORD)base + InCondOffset);
-    //Function = (oFunction)(sig);
+	auto base = GetModuleHandleA("client.dll");
 
-    DetourTransactionBegin();
-    DetourAttach(&(PVOID&)Function, CTFPlayerShared_InCond);
+	oInCond = (tInCond)((DWORD)base + InCondOffset);
+
+
+	DetourTransactionBegin();
+	DetourAttach(&(PBOOL&) oInCond, hInCond);
 	DetourTransactionCommit();
 
-	return 69;
+	return 3133;
 }
 
 BOOL WINAPI DllMain(
-    _In_      HINSTANCE hinstdll,
-    _In_      DWORD     fdwreason,
-    _In_opt_  LPVOID    lpvreserved)
+	_In_      HINSTANCE hinstdll,
+	_In_      DWORD     fdwreason,
+	_In_opt_  LPVOID    lpvreserved)
 {
-    switch (fdwreason) {
-    case DLL_PROCESS_ATTACH:
-        DisableThreadLibraryCalls(hinstdll);
-        CreateThread(nullptr, 0, entry, hinstdll, 0, nullptr);
-        return TRUE;
-    case DLL_PROCESS_DETACH:
-        FreeLibraryAndExitThread(hinstdll, 0);
-        return TRUE;
-    default:
-        return TRUE;
-    }
+	switch (fdwreason) {
+	case DLL_PROCESS_ATTACH:
+		DisableThreadLibraryCalls(hinstdll);
+		CreateThread(nullptr, 0, entry, hinstdll, 0, nullptr);
+		return TRUE;
+	case DLL_PROCESS_DETACH:
+		FreeLibraryAndExitThread(hinstdll, 0);
+		return TRUE;
+	default:
+		return TRUE;
+	}
 }
