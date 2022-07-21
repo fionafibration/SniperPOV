e604c5c7dba6e610058cbb82920ac9b185beaf0f
Fiona <spamsuckersunited@gmail.com>
2022-07-21 14:29:55 -0600

    attempting signature matching

--- SniperPOV/SniperPOV/main.cpp
+++ SniperPOV/SniperPOV/main.cpp
@@ -2,6 +2,8 @@
 
 #include <Windows.h>
 #include <format>
+#include <Psapi.h>
+#include <TlHelp32.h>
 
 #pragma comment (lib, "detours.lib")
 #include "detours.h"
@@ -11,10 +13,43 @@
 
 using namespace std;
 
+DWORD FindPattern(const char* pattern, const char* mask)
+{
+
+	MODULEINFO info = {};
+	auto hmod = GetModuleHandle("client.dll");
+	if (hmod == nullptr) {
+		return NULL;
+	}
+	GetModuleInformation(GetCurrentProcess(), hmod, &info, sizeof(MODULEINFO));
+	DWORD base = (DWORD)info.lpBaseOfDll;
+	DWORD size = (DWORD)info.SizeOfImage;
+
+	DWORD patternLength = (DWORD)strlen(mask);
+
+	for (DWORD i = 0; i < size - patternLength; i++)
+	{
+		bool found = true;
+		for (DWORD j = 0; j < patternLength; j++)
+		{
+			found &= mask[j] == '?' || pattern[j] == *(char*)(base + i + j);
+		}
+
+		if (found)
+		{
+			return base + i;
+		}
+	}
+
+	return NULL;
+}
+
 
 DWORD InCondOffset = 0x4c17d0;
 DWORD WearableDrawOffset = 0x3227f0;
 DWORD PlayerShouldDrawOffset = 0x409db0;
+
+DWORD base;
 
 
 typedef bool(__thiscall* tInCond) (void* ths, ETFCond cond);
@@ -22,28 +57,34 @@ tInCond oInCond;
 
 bool __fastcall hInCond(void* ecx, void* edx, ETFCond cond) {
 
-	//auto caller = _ReturnAddress();
-	//if (caller == (base + WearableDrawOffset) { return false; }
-	//if (caller == (base + PlayerShouldDrawOffset) { return false; }
-	//return Function(ecx, cond);
 	MessageBox(NULL, "Hooked Function!", "Sniper POV", MB_SYSTEMMODAL);
+
+	if ((DWORD) _ReturnAddress() == (base + WearableDrawOffset)) { return false; }
+	if ((DWORD) _ReturnAddress() == (base + PlayerShouldDrawOffset)) { return false; }
+	return oInCond(ecx, cond);
+
 	return false;
 
-
 }
-
-
 
 DWORD WINAPI entry(LPVOID lpparam)
 {
 
 	//auto sig = find_ida_sig("client.dll", { "55 8B EC 83 EC 08 56 57 8B 7D 08 8B F1 83 FF 20" });
-	//Function = (oFunction)(sig);
+	auto sig = FindPattern("\x55\x8B\xEC\x83\xEC\x08\x56\x57\x8B\x7D\x08\x8B\xF1\x83\xFF\x20", "xxxxxxxxxxxxxxxx");
+	oInCond= (tInCond)(sig);
 
 
-	auto base = GetModuleHandle("client.dll");
 
-	oInCond = (tInCond)((DWORD)base + InCondOffset);
+	base = (DWORD) GetModuleHandle(nullptr);
+
+	auto addr = (base + InCondOffset);
+
+	oInCond = (tInCond)(addr);
+
+	if (true) {
+
+	}
 
 	DetourTransactionBegin();
 	DetourUpdateThread(GetCurrentThread());
@@ -58,6 +99,7 @@ DWORD WINAPI entry(LPVOID lpparam)
 
 	return 3133;
 }
+
 
 void do_entry_thread(HINSTANCE hinstdll) {
 	// https://docs.microsoft.com/en-us/windows/win32/dlls/dynamic-link-library-best-practices
