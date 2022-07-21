e1efc59ea313b4892be012810009b7c25af47930
Fiona <spamsuckersunited@gmail.com>
2022-07-21 15:06:16 -0600

    should be working

--- SniperPOV/SniperPOV/main.cpp
+++ SniperPOV/SniperPOV/main.cpp
@@ -13,7 +13,7 @@
 
 using namespace std;
 
-DWORD FindPattern(const char* pattern, const char* mask)
+DWORD search_pattern(const char* pattern, const char* mask)
 {
 
 	MODULEINFO info = {};
@@ -22,15 +22,16 @@ DWORD FindPattern(const char* pattern, const char* mask)
 		return NULL;
 	}
 	GetModuleInformation(GetCurrentProcess(), hmod, &info, sizeof(MODULEINFO));
+
 	DWORD base = (DWORD)info.lpBaseOfDll;
 	DWORD size = (DWORD)info.SizeOfImage;
 
-	DWORD patternLength = (DWORD)strlen(mask);
+	DWORD len = (DWORD)strlen(pattern);
 
-	for (DWORD i = 0; i < size - patternLength; i++)
+	for (DWORD i = 0; i < size - len; i++)
 	{
 		bool found = true;
-		for (DWORD j = 0; j < patternLength; j++)
+		for (DWORD j = 0; j < len; j++)
 		{
 			found &= mask[j] == '?' || pattern[j] == *(char*)(base + i + j);
 		}
@@ -44,12 +45,12 @@ DWORD FindPattern(const char* pattern, const char* mask)
 	return NULL;
 }
 
+//DWORD base;
 
-DWORD InCondOffset = 0x4c17d0;
-DWORD WearableDrawOffset = 0x3227f0;
-DWORD PlayerShouldDrawOffset = 0x409db0;
 
-DWORD base;
+// Return addresses of the wearable and player draw functions so we can say no to sniper zoom
+DWORD wearable_draw;
+DWORD player_draw;
 
 
 typedef bool(__thiscall* tInCond) (void* ths, ETFCond cond);
@@ -57,34 +58,37 @@ tInCond oInCond;
 
 bool __fastcall hInCond(void* ecx, void* edx, ETFCond cond) {
 
-	MessageBox(NULL, "Hooked Function!", "Sniper POV", MB_SYSTEMMODAL);
+	//MessageBox(NULL, "Hooked Function!", "Sniper POV", MB_SYSTEMMODAL);
 
-	if ((DWORD) _ReturnAddress() == (base + WearableDrawOffset)) { return false; }
-	if ((DWORD) _ReturnAddress() == (base + PlayerShouldDrawOffset)) { return false; }
+	if (cond == TFCond_Zoomed) {
+		if ((DWORD)_ReturnAddress() == wearable_draw) { return false; }
+		if ((DWORD)_ReturnAddress() == player_draw) { return false; }
+	}
+
 	return oInCond(ecx, cond);
-
-	return false;
 
 }
 
 DWORD WINAPI entry(LPVOID lpparam)
 {
 
-	//auto sig = find_ida_sig("client.dll", { "55 8B EC 83 EC 08 56 57 8B 7D 08 8B F1 83 FF 20" });
-	auto sig = FindPattern("\x55\x8B\xEC\x83\xEC\x08\x56\x57\x8B\x7D\x08\x8B\xF1\x83\xFF\x20", "xxxxxxxxxxxxxxxx");
+	auto sig = search_pattern("\x55\x8B\xEC\x83\xEC\x08\x56\x57\x8B\x7D\x08\x8B\xF1\x83\xFF\x20", "xxxxxxxxxxxxxxxx");
+	
+	// Find the CALL instructions to the incond and add 5 to get the address they will be returning to
+
+	wearable_draw = search_pattern("\xE8????\x84\xC0\x75\xE1\x6A\x03", "x????xxxxxx") + 5;
+	
+	player_draw = search_pattern("\xE8????\x84\xC0\x75\xC5", "x????xxxx") + 5;
+
 	oInCond= (tInCond)(sig);
 
 
 
-	base = (DWORD) GetModuleHandle(nullptr);
+	//base = (DWORD) GetModuleHandle("client.dll");
 
-	auto addr = (base + InCondOffset);
+	//auto addr = (base + InCondOffset);
 
-	oInCond = (tInCond)(addr);
-
-	if (true) {
-
-	}
+	//oInCond = (tInCond)(addr);
 
 	DetourTransactionBegin();
 	DetourUpdateThread(GetCurrentThread());
@@ -97,7 +101,7 @@ DWORD WINAPI entry(LPVOID lpparam)
 		return -1;
 	}
 
-	return 3133;
+	return 0;
 }
 
 
